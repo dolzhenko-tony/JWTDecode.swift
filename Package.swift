faea3699f5c68a60ff8cc41296b5ebcef8f03c91
Anton Dolzhenko <dolzhenko.tony@gmail.com>
2020-06-16 16:05:41 +0300

    remove testTarget to kill Quick and Nimble

--- Package.swift
+++ Package.swift
@@ -11,20 +11,12 @@ let package = Package(
             name: "JWTDecode",
             targets: ["JWTDecode"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-         .package(url: "https://github.com/Quick/Quick", from: "2.1.0"),
-         .package(url: "https://github.com/Quick/Nimble", from: "8.0.0")
-    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "JWTDecode",
             dependencies: [],
-            path: "JWTDecode"),
-        .testTarget(
-            name: "JWTDecode.swiftTests",
-            dependencies: ["JWTDecode", "Quick", "Nimble"]),
+            path: "JWTDecode")
     ]
 )
